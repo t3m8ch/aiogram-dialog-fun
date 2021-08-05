55897534f428c64900f447d18a8ba4867f782df9
Kudyakov Artem <t3m8ch@yandex.ru>
2021-08-05 13:55:00 +0400

    feat(dialogs): Use getter

--- app/bot/dialogs/main_dialog.py
+++ app/bot/dialogs/main_dialog.py
@@ -1,17 +1,24 @@
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram_dialog import Window, Dialog
 from aiogram_dialog.widgets.kbd import Button
-from aiogram_dialog.widgets.text import Const
+from aiogram_dialog.widgets.text import Const, Format
 
 
 class MySG(StatesGroup):
     main = State()
 
 
+async def get_data(**kwargs):
+    return {
+        "name": "T3M8CH"
+    }
+
+
 main_window = Window(
-    Const("Hello, %username%!"),
+    Format("Hello, {name}"),
     Button(Const("Useless button"), id="nothing"),
-    state=MySG.main
+    state=MySG.main,
+    getter=get_data
 )
 
 main_dialog = Dialog(main_window)
