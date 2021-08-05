8dd697e7a74a61248bb94cc01918ec26cebc4656
Kudyakov Artem <t3m8ch@yandex.ru>
2021-08-05 14:08:44 +0400

    feat(dialog): Use Case

--- app/bot/dialogs/main_dialog.py
+++ app/bot/dialogs/main_dialog.py
@@ -1,22 +1,34 @@
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram_dialog import Window, Dialog
 from aiogram_dialog.widgets.kbd import Button
-from aiogram_dialog.widgets.text import Const, Format
+from aiogram_dialog.widgets.text import Const, Format, Case
 
 
 class MySG(StatesGroup):
     main = State()
 
 
+text = Case(
+    {
+        "red": Const("Square"),
+        "green": Const("Unicorn"),
+        "blue": Const("Moon"),
+    },
+    selector="color"
+)
+
+
 async def get_data(**kwargs):
     return {
-        "name": "T3M8CH"
+        "name": "T3M8CH",
+        "color": "red"
     }
 
 
 main_window = Window(
     Format("Hello, {name}"),
     Button(Const("Useless button"), id="nothing"),
+    text,
     state=MySG.main,
     getter=get_data
 )
