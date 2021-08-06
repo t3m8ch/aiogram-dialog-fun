7e00541f22f8eecb79be506464731da6d53af8e1
Kudyakov Artem <t3m8ch@yandex.ru>
2021-08-06 09:27:39 +0400

    feat: Use Next and Back buttons

--- app/bot/dialogs/menu_dialog.py
+++ app/bot/dialogs/menu_dialog.py
@@ -7,12 +7,14 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.types import CallbackQuery
 from aiogram_dialog import Window, Dialog, DialogManager, ChatEvent
 from aiogram_dialog.widgets.kbd import Button, Group, Checkbox, Radio, Select, \
-    Multiselect
+    Multiselect, Next, Row, Back
 from aiogram_dialog.widgets.text import Const, Format, Case
 
 
 class MySG(StatesGroup):
     main = State()
+    second = State()
+    third = State()
 
 
 color_text = Case(
@@ -56,7 +58,7 @@ async def on_random_number(call: types.CallbackQuery,
 async def on_check_state_change(event: ChatEvent, checkbox: Checkbox,
                                 manager: DialogManager):
     logging.debug(
-        "-"*30 + f" Check status changed: {checkbox.is_checked(manager)}"
+        "-" * 30 + f" Check status changed: {checkbox.is_checked(manager)}"
     )
 
 
@@ -65,41 +67,57 @@ async def on_language_selected(call: CallbackQuery, select: Select,
     await call.message.answer(f"Вы выбрали: {item}")
 
 
-menu_window = Window(
-    Format("Hello, {name}"),
-    Group(
-        Button(Const("Useless button"), id="nothing"),
-        Button(Const("Random name"), id="randname", on_click=on_random_name),
-        Button(Const("Random number"), id="randnum", on_click=on_random_number),
-        width=2
+menu_dialog = Dialog(
+    Window(
+        Format("Hello, {name}"),
+        Group(
+            Button(Const("Useless button"), id="nothing"),
+            Button(Const("Random name"), id="randname",
+                   on_click=on_random_name),
+            Button(Const("Random number"), id="randnum",
+                   on_click=on_random_number),
+            width=2
+        ),
+        Checkbox(
+            Const("✅ On"),
+            Const("❌ Off"),
+            id="check",
+            default=True,
+            on_state_changed=on_check_state_change
+        ),
+        Radio(
+            Format("🔘 {item}"),
+            Format("⚪️ {item}"),
+            id="r_languages",
+            item_id_getter=lambda item: item,
+            items="languages",
+            on_state_changed=on_language_selected,
+        ),
+        Multiselect(
+            Format("✓ {item[0]}"),
+            Format("{item[0]}"),
+            id="m_fruits",
+            item_id_getter=operator.itemgetter(1),
+            items="fruits",
+            max_selected=2,
+            when=lambda data, _, __: data["name"] == "T3M8CH"
+        ),
+        Next(Const(">>")),
+        color_text,
+        state=MySG.main,
+        getter=get_data
     ),
-    Checkbox(
-        Const("✅ On"),
-        Const("❌ Off"),
-        id="check",
-        default=True,
-        on_state_changed=on_check_state_change
+    Window(
+        Const("second"),
+        Row(
+            Back(Const("<<")),
+            Next(Const(">>"))
+        ),
+        state=MySG.second,
     ),
-    Radio(
-        Format("🔘 {item}"),
-        Format("⚪️ {item}"),
-        id="r_languages",
-        item_id_getter=lambda item: item,
-        items="languages",
-        on_state_changed=on_language_selected,
-    ),
-    Multiselect(
-        Format("✓ {item[0]}"),
-        Format("{item[0]}"),
-        id="m_fruits",
-        item_id_getter=operator.itemgetter(1),
-        items="fruits",
-        max_selected=2,
-        when=lambda data, _, __: data["name"] == "T3M8CH"
-    ),
-    color_text,
-    state=MySG.main,
-    getter=get_data
+    Window(
+        Const("third"),
+        Back(Const("<<")),
+        state=MySG.third
+    )
 )
-
-menu_dialog = Dialog(menu_window)
